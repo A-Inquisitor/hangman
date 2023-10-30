983b374ee913de925266747d3552bcd963f6bb1c
A-Inquisitor <xyz.quantum1@gmail.com>
2023-10-30 11:54:43 +0000

    Cleaning up a few things

--- milestone_5.py
+++ milestone_5.py
@@ -29,7 +29,9 @@ class hangman:
             self.num_lives = num_lives
             self.word_list = word_list
             self.list_guesses = []
-
+    '''
+    Initialise all the required self functions, these will be used later on
+    '''
     def check_guess(self, guess):
         guess = guess.lower()
         if guess in word:
@@ -37,13 +39,15 @@ class hangman:
             for letter in word:
                 if self.word[letter] == guess:
                     self.word_guessed[letter] = guess
-            self.num_letter =- 1
+            self.num_letter -= 1
         else:
-            self.num_lives =- 1
+            self.num_lives -= 1
             print(f"Sorry, {letter} is not in the word. Try again.")
             print(f"You have {self.num_lives} lives left.")
             print(f"Sorry, {guess} is not in the word. Try again.")
-    
+    '''
+    This checks if the guess is actually in the word, if it is not in the word it will print statements to say that the user has progressed a certain amount
+    '''
     def ask_for_input(self):
         while True:
             guess = input("Guess a letter: ") 
@@ -55,7 +59,9 @@ class hangman:
                 self.check_guess(guess)
                 self.list_guesses.append(guess)
                 print("Your valid guess is: ", guess)
-
+    '''
+    This is the function that asks for the users input, and helps with things like repeated letters etc
+    '''
     def play_game(self):
         num_lives = 5 
         hangman_game = hangman(word_list)
@@ -66,6 +72,9 @@ class hangman:
                 self.ask_for_input()
             else:
                 print("Congratulations. You won the game")
+    '''
+    This function allows for the game to actually be played
+    '''
 
 favourite_fruits = ["banana", "apple", "kiwi", "Mango", "Grapes"]
 word_list = favourite_fruits
